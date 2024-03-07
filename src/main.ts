bb4988d933e43996b038d7715183c495aca1b233
zhaohs <1787600886@qq.com>
2024-03-07 13:21:11 +0800

    2024-03-07 13:21:06 | auto push

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { log } from 'console';
-import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger'
-import {ValidationPipe} from '@nestjs/common'
+import fs from 'fs'
+import * as https from "https";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+const httpsOptions = {
+  ca: fs.readFileSync('root_bundle.crt文件地址'),
+  key: fs.readFileSync('.key文件地址'),
+  cert: fs.readFileSync('另一个.crt文件地址'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
+  const app = await NestFactory.create(AppModule, { cors: true });
   //swagger
   const config = new DocumentBuilder()
     .setTitle('zhaohs swagger')
@@ -15,13 +23,16 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('doc', app, document);
-  app.useGlobalPipes(new ValidationPipe({
-    transform:true,
-    transformOptions:{
-      enableImplicitConversion:true
-    }
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
+  https.createServer(httpsOptions, server).listen(443);
   log('http://localhost:3000');
 }
 bootstrap();
